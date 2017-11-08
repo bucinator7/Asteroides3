8fb4cd625203e3a7fa31e2f26e24258779746f71
mariojiri <mariojimenezridruejo@gmail.com>
2017-11-08 21:50:44 +0100

    Initial commit

---
+++ app/src/main/java/example/org/asteroides3/AcercaDeActivity.java
@@ -0,0 +1,17 @@
+package example.org.asteroides3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Vespertino on 08/11/2017.
+ */
+
+public class AcercaDeActivity extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acercade);
+    }
+}
